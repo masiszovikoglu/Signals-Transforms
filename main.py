e94ec7bc5086f21d7a54bf4eab81813686bde3b6
XanderHeij <73619224+XanderHeij@users.noreply.github.com>
2022-06-21 13:16:31 +0200

    this code should work with correct data

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from math import sin
 import numpy as np
 from excel import get_data
 from matplotlib import pyplot as plt
-from numpy.fft import fft, ifft
+from numpy.fft import fft
 
 # temporary
 def get_x(values):
@@ -13,43 +13,53 @@ def get_x(values):
 
 def main():
     #x = [0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1]
-    x = []
-    for i in range(500):
-        x.append(np.sin(i/10)+np.sin(i/1.5))
+    # x = []
+    # for i in range(500):
+    #     x.append(np.sin(i/10)+np.sin(i/1.5))
     
    
-    # my_dict = get_data()
-    # x = []
-    # for i in my_dict["Value"]:
-    #     x.append(int(i))
+    Tepoch = 1
+    my_dict = get_data()
+    f = []
+    for i in my_dict["Value"]:
+        f.append(int(i))
+        Tepoch += 10
+    Ts = Tepoch / len(f) 
+    #Ts = 0.01
+    t = np.arange(0, Tepoch, Ts) #+Ts/2
+    # f = np.cos(2*np.pi*5*t)+np.cos(2*np.pi*10*t)/2
+    N = len(f)
+    M = int(2**(np.ceil(np.log(N)/np.log(2))+4))
+    w = np.arange((M//2)+1)*2*np.pi/M/Ts
+    fhat = fft(f,M)*Ts
+    fhat = fhat[0:(M//2+1)]
 
-    # sr = len(x)
-    # ts = 1/sr
-    # t = np.arange(0,1,ts)/2
-    #t = np.arange(0, sr, 1)
-    print(len(x))
+    plt.plot(w/(2*np.pi), abs(fhat))
+    plt.xlabel('frequency $\omega/(2\pi)$ [t]$^{-1}$')
+    plt.ylabel('unit: $[f][t]$')
+    plt.grid()
+    #plt.savefig('code1p1.pdf') # save it as pdf
+    plt.show() 
+    l = len(t) / 50
+    for i in range(50):
+        plt.plot(t[int(l*i):int(l*(i+1))], f[int(l*i):int(l*(i+1))])
+        plt.grid()
+        plt.show()
+
+    # print(len(x))
     
-    #plt.plot(t[0:1440], x[0:1440])
-    plt.plot(np.arange(0,500,1), x[0:500])
-    plt.show()
-    # plt.plot(t[9000:10000], x[9000:10000])
+    # plt.plot(np.arange(0,500,1), x[0:500])
     # plt.show()
     
-    fs = 1
-    T = 1/fs
-    N = len(x)
-    yf = fft(x)
-    xf = np.linspace(0.0, fs / 2, N//2)
+    # fs = 6*24
+    # T = 1/fs
+    # N = len(x)
+    # yf = fft(x)
+    # xf = np.linspace(0.0, fs / 2, N//2)
     
-    plt.plot(xf, 2/N * np.abs(yf[:N//2]))
-    plt.show()
-
-    # x_new = ifft(X)
-    # # for i in range(len(x)):
-    # #     if x[i] != x_new[i]:
-    # #         print(x[i], x_new[i])
-    # plt.plot(t[0:1440], x_new[0:1440])
+    # plt.plot(xf, 2/N * np.abs(yf[:N//2]))
     # plt.show()
+
 
 if __name__ == "__main__":
     main()
