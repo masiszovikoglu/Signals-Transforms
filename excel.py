567d485f53bdc6072dd2a3f37cfeaf98779f1e92
XanderHeij <73619224+XanderHeij@users.noreply.github.com>
2022-06-24 20:22:12 +0200

    peaks could work now

--- excel.py
+++ excel.py
@@ -6,6 +6,7 @@ import pandas as pd
 import os
 
 import openpyxl
+import numpy as np
  
 # Give the location of the file
 path = "water.xlsx"
@@ -19,17 +20,27 @@ my_dict = {"Date":[],"Value":[]};
 max_col = sheet_obj.max_column
 # number of rows of excel file
 m_row = sheet_obj.max_row
+
 for i in range(2, m_row + 1):
     cell_obj = sheet_obj.cell(row = i, column = 1)
     x=cell_obj.value.split()
     line=x[12].split(";")
     date=line[4]+" "+ line[5]
-    height=line[7]
-    
+    height=int(line[7])
     my_dict["Date"].append(date)
     my_dict["Value"].append(height)
 # for i in range(len(my_dict["Date"])):    
 #     print ("Date"+ my_dict["Date"][i] +" = " + my_dict["Value"][i])   
-    
+for i in range(len(my_dict["Value"])):
+    if my_dict["Value"][i] > 1000000:
+        j = i
+        while True:
+            j += 1
+            if my_dict["Value"][j] <= 1000000:
+                break
+        arr = np.arange(my_dict["Value"][i-1], my_dict["Value"][j+1], (my_dict["Value"][j+1] - my_dict["Value"][i-1]) / (j - i + 2))
+        for k in range(i, j):
+            my_dict["Value"][k] = int(arr[k + 1 - i])
+
 def get_data():
     return my_dict 
--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from math import sin
+from pydoc import doc
 import numpy as np
 from excel import get_data
 from matplotlib import pyplot as plt
 from numpy.fft import fft
+from scipy.signal import find_peaks
+
+
+# def get_n_highest_peaks(arr):
+#     peaks = []
+#     i = 0
+#     l = len(arr)
+#     while True:
+#         if i + 1 == l or arr[i+1] < arr[i]:
+#             break
+#         else:
+#             i += 1
+#     peaks.append(arr[i])
+#     last = arr[i]
+#     new = False
+#     for j in range(i, l):
+#         if not new and arr[j] < 0.8 * last:
+#             new = True
+#         if new and j + 1 < l and arr[j + 1] < arr[j]:
+#             peaks.append(arr[j])
+#             last = arr[j]
+#             new = False
+#     return peaks
 
 # temporary
 def get_x(values):
@@ -18,33 +42,47 @@ def main():
     #     x.append(np.sin(i/10)+np.sin(i/1.5))
     
    
-    Tepoch = 1
+    Tepoch = 0
     my_dict = get_data()
     f = []
     for i in my_dict["Value"]:
         f.append(int(i))
-        Tepoch += 10
-    Ts = Tepoch / len(f) 
-    #Ts = 0.01
-    t = np.arange(0, Tepoch, Ts) #+Ts/2
-    # f = np.cos(2*np.pi*5*t)+np.cos(2*np.pi*10*t)/2
+        Tepoch += 10    
     N = len(f)
+    Ts = Tepoch / N 
+    t = np.arange(0, Tepoch, Ts) 
     M = int(2**(np.ceil(np.log(N)/np.log(2))+4))
-    w = np.arange((M//2)+1)*2*np.pi/M/Ts
+    w = (np.arange((M//2)+1)*6*24)/M
+    
     fhat = fft(f,M)*Ts
     fhat = fhat[0:(M//2+1)]
+    peaks, _ = find_peaks(abs(fhat)[0:len(fhat)//3])
+    peaksxy = []
+    print("A")
+    for p in peaks:
+        peaksxy.append((p, abs(fhat)[p]))        
+    print("B")
+    peaksxy = sorted(peaksxy, key=lambda x: x[1], reverse=True)[0:10]
 
-    plt.plot(w/(2*np.pi), abs(fhat))
+    plt.plot(w, abs(fhat))
+    plt.scatter(list(x[0]*(6*24)/M for x in peaksxy), list(y[1] for y in peaksxy), color="red") 
     plt.xlabel('frequency $\omega/(2\pi)$ [t]$^{-1}$')
     plt.ylabel('unit: $[f][t]$')
     plt.grid()
     #plt.savefig('code1p1.pdf') # save it as pdf
     plt.show() 
-    l = len(t) / 50
-    for i in range(50):
-        plt.plot(t[int(l*i):int(l*(i+1))], f[int(l*i):int(l*(i+1))])
-        plt.grid()
-        plt.show()
+
+    plt.plot(t / 60, f)
+    plt.xlabel('time in hours')
+    plt.ylabel('height in cm')
+    plt.grid()
+    plt.show()
+    
+    # l = len(t) / 50
+    # for i in range(50):
+    #     plt.plot(t[int(l*i):int(l*(i+1))], f[int(l*i):int(l*(i+1))])
+    #     plt.grid()
+    #     plt.show()
 
     # print(len(x))
     
@@ -53,11 +91,12 @@ def main():
     
     # fs = 6*24
     # T = 1/fs
-    # N = len(x)
-    # yf = fft(x)
+    # N = len(f)
+    # yf = fft(f)
     # xf = np.linspace(0.0, fs / 2, N//2)
     
     # plt.plot(xf, 2/N * np.abs(yf[:N//2]))
+    # plt.grid()
     # plt.show()
 
 
